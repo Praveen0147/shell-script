677a0dd112defcb3d3bf056d7f6a4fa728f47a83
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-12 19:06:31 +0530

    dynamic variables testing

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1 #This is to accept first input during CLI
+PERSON2=$2 #This is to accept second input during CLI
+
+echo "Hello World , This is for learning of AWS & Devops"
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hello $PERSON1"
