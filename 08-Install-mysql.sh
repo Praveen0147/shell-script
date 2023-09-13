90392cc91f8c88062d42bfbe98ae5c4576c6af97
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-13 21:32:48 +0530

    Updated conditions logic

---
+++ 08-Install-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+   echo " Run with Sudo Access"
+   exit 2
+fi
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+   echo " Installation is not succesful"
+   exit 3
+else 
+   echo "Installation success"
+ fi  
