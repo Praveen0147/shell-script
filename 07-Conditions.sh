43803fa96a30ec9db7080e740277902a6fa15452
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-13 19:11:00 +0530

    07-consitions

---
+++ 07-Conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This code is to print number ge/equal 10 or not
+NUMBER=$1
+if [$number -ge 10]
+then
+  echo " Given Number is greater/equal to 10"
+else
+   echo " Given Number is less than 10 "
+if    
