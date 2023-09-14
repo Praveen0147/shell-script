94e429f4d9de2356dad81d9c2d5a95ee22283da7
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-14 12:37:41 +0530

    Functions code

---
+++ 09-Functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USERID=$(id -u)
+VALIDATE ()
+{
+  if [ $? -ne 0 ]
+  then 
+     echo "$2 .. Fail"
+  else 
+     echo "$2 ... Pass"   
+}
+if [ $USERID -ne 0 ]
+then 
+   echo " Run with Sudo Access"
+   exit 2
+fi
+yum install mysql -y
+VALIDATE $? "MYSQL INSTALLATION"
+yum install postfix -y
+VALIDATE $? "Postfix INSTALLATION"
