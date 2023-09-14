6638a981efcbf2bf261df8e0ecdc77aa218fb709
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-14 18:12:29 +0530

    complete foor loop push

--- 14-complete-loop.sh
+++ 14-complete-loop.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 for i in $@
 do
-    yum install $i -y
+    package_name=$i
+    rpm -q "$package_name"
+    if [ $? eq 0 ] then
+      echo -e "$G $i already installed"
+    else   
+        yum install $i -y
+        if [ $? -eq 0 ]
+        echo -e "$G $i installation successful"
+        else
+        echo -e "$R $i installation fail"
+        fi
+    fi    
 done    
