ba2a727ada78e77f6494a3cbbbe394e5aaf276ee
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-14 13:40:08 +0530

    Functions code if rectified

---
+++ 11-logfile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$CRIPT-NAME-$DATE.log
+USERID=$(id -u)
+VALIDATE ()
+{
+  if [ $? -ne 0 ]
+  then 
+     echo "$2 .. Fail"
+  else 
+     echo "$2 ... Pass"  
+  fi    
+}
+if [ $USERID -ne 0 ]
+then 
+   echo " Run with Sudo Access"
+   exit 2
+fi
+yum install mysql -y &>>$LOGFILE
+VALIDATE $? "MYSQL INSTALLATION"
+yum install postfix -y &>>$LOGFILE
+VALIDATE $? "Postfix INSTALLATION"
