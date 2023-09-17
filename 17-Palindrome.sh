b4f682d2076fbfd362dc586510ca9d0cf037fb41
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-17 21:38:45 +0530

    palindrome change script

--- 17-Palindrome.sh
+++ 17-Palindrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo " Enter a word"
 read -s WORD
+echo " word entered is $WORD"
 #word1=$(echo $1 | tr -d ' ' | tr 'A-Z' 'a-z')
 Reverse=$(echo "$WORD" |rev)
 if ["$WORD" = "$Reverse" ]
