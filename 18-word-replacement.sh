16bb9ac99780f4e60fa647fb135ae3febdc995de
Praveen0147 <praveenkumar.sudabathula0147@gmail.com>
2023-09-17 22:04:24 +0530

    word replacement

--- 18-word-replacement.sh
+++ 18-word-replacement.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-File1=$1
-old_word=$2
-new_word=$3
-sed -i "s/.*/$old_word/$new_word/g" "$File1"
+File1="$1"
+old_word="$2"
+new_word="$3"
+sed -i "s/$old_word/$new_word/g" "$File1"
 echo "Content of $File1 Replaced, Please check"
